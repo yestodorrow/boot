234c7dc05c93713276a0cf58c370d341d778ae87
yestodorrow <power998@163.com>
2018-03-25 15:44:44 +0800

    demo for vue bootstrap

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="hello">
+
+    <b-container class="bv-example-row">
+      
+        <b-breadcrumb :items="items"/>
+     <div class="row">
+           <b-carousel id="carousel1"
+                style="text-shadow: 1px 1px 2px #333;"
+                controls
+                indicators
+                background="#ababab"
+                :interval="4000"
+                img-width="1024"
+                img-height="480"
+                v-model="slide"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+    >
+
+      <!-- Text slides with image -->
+      <b-carousel-slide caption="First slide"
+                        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+                        img-src="https://lorempixel.com/1024/480/technics/2/"
+      ></b-carousel-slide>
+
+      <!-- Slides with custom text -->
+      <b-carousel-slide img-src="https://lorempixel.com/1024/480/technics/4/">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide img-src="https://lorempixel.com/1024/480/technics/8/">
+      </b-carousel-slide>
+
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <img slot="img" class="d-block img-fluid w-100" width="1024" height="480"
+             src="https://lorempixel.com/1024/480/technics/5/" alt="image slot">
+      </b-carousel-slide>
+
+      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut pellentesque
+          ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+
+    </b-carousel>
+
+     </div>
+
+      <div class="row">
+    <template v-for="variant in ['primary','secondary','success','outline-success','warning','danger','link']">
+        <div class="col-md-4 pb-2" v-for="size in ['sm','','lg']" :key="`${variant}_${size}`">
+            <b-button :size="size" :variant="variant">
+                {{variant}} {{size}}
+            </b-button>
+        </div>
+        
+    </template>
+    <b-form-file v-model="file" :state="Boolean(file)" placeholder="Choose a file..."></b-form-file>
+ 
+  <div>
+  <b-img src="https://lorempixel.com/1024/400/" fluid alt="Responsive image" />
+
+
+  
+</div>
+</div>
+    </b-container>
+
+
+    <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+ 
+    </b-row>
+
+
+
+    <div>
+  <b-btn v-b-modal.modal1>Launch demo modal</b-btn>
+
+  <!-- Modal Component -->
+  <b-modal id="modal1" title="Bootstrap-Vue">
+    <p class="my-4">Hello from modal!</p>
+  </b-modal>
+</div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      isRed:true,
+      items: [
+  {
+    text: 'Home',
+    href: 'http://google.com',
+  }, {
+    text: 'Posts',
+    to: { name: 'home' },
+  }, {
+    text: 'Another Story',
+    active: true
+  }
+]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.red{
+  color:red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+
+import BoostrapVue from 'bootstrap-vue'
+
+Vue.use(BoostrapVue)
+
+
+
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
